d356ed8ba577ab2b214aaf27d853dce03c39b5ca
teeotsa <78772453+teeotsa@users.noreply.github.com>
2021-11-07 02:02:33 +0200

    v1.6
    Added new icon for GUI (You can find raw png file from bin folder)
    Added Dark Mode to PowerShell
    Remade GUI and added some restore buttons
    Fixed some bugs
    Restore buttons doesn't work right now. Functions will be added later, you have to use Restore Points
    Did alot of tweaking

--- bin/modules/forceRemove.psm1
+++ bin/modules/forceRemove.psm1
@@ -1,14 +1,25 @@
-function TakeOwnership{
+<#
+
+    This module can break your system when used wrong!
+
+#>
+
+function Take-Ownership{
     [CmdletBinding()]
     param(
         [Parameter()]
-        [string]$Path
+        [string] $Path
     )
-    if (!(Test-Path $Path)){
-        write-Warning "`"$Path`" does not exist!";
-            return
+    if ($Path.Length -lt 1){
+        $SystemTime = Get-Date -DisplayHint Time
+        Write-Host "$SystemTime | Hey! You can't input empty path!" -ForegroundColor Red
+        return;
     }
-
+    if (!(Test-Path $Path)){
+        $SystemTime = Get-Date -DisplayHint Time
+        Write-Host "$SystemTime | Can't find path!" -ForegroundColor Red
+        return;
+    }
     takeown /f $Path /r | Out-Null
     $User = $env:USERNAME
     icacls $Path /grant Everyone:F /Q /C /T | Out-Null
